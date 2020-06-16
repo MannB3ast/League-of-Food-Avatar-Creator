ac33435e88d739ad7126afe37cce5712f551b283
Mann, Eustace (Contractor) <Eustace_Mann@comcast.com>
2020-06-15 23:19:19 -0400

    Initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 EulersPerSecond;
+
+	void Update()
+	{
+		transform.Rotate(EulersPerSecond * Time.deltaTime);
+    }
+}
