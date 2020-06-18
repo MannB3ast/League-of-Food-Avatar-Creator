9ffa3703de0b6960ac213d14271dbd412976ad25
Mann, Eustace (Contractor) <Eustace_Mann@comcast.com>
2020-06-18 10:04:32 -0400

    Scale button on hover

---
+++ Assets/ChangeScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    public float scaleShiftFactor = 1.06f;
+    private Vector3 defaultPos;
+    private void Start() {
+        defaultPos = transform.position;
+    }
+    public void ScaleUp(){
+        GetComponent<RectTransform>().sizeDelta *= 1.25f;
+        Vector3 newPos = transform.localPosition;
+        newPos.x *= scaleShiftFactor;
+        transform.localPosition = newPos;
+    }
+
+    public void ScaleDown(){
+        GetComponent<RectTransform>().sizeDelta /= 1.25f;
+        transform.position = defaultPos;
+    }
+}
