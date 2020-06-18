ce988f4632c051c5d923787d14041e0019cbcefc
Mann, Eustace (Contractor) <Eustace_Mann@comcast.com>
2020-06-18 13:36:27 -0400

    Added background changer
    - Added background changer

---
+++ Assets/ChangeBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBackground : MonoBehaviour
+{
+    public List<Sprite> bgImages = new List<Sprite>();
+    int index = 0;
+    public void ChangeBG(){
+        if(index < bgImages.Count - 1){
+                index++;
+            } else {
+                index = 0;
+            }
+            UpdateBackground();
+    }
+
+    public void UpdateBackground(){
+        GetComponent<Image>().sprite = bgImages[index];
+    }
+}
--- Assets/Scripts/ChangeGritty.cs
+++ Assets/Scripts/ChangeGritty.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.U2D.Animation;
-using UnityEngine.UI;
 
 namespace Unity.U2D.Animation.Sample
 {
     public class ChangeGritty : MonoBehaviour
     {
         private SpriteResolver resolver;
-        private SpriteLibraryAsset currentSpriteLibrary;
+        List<string> catList = new List<string>();
+        List<string> labelList = new List<string>();
+        int catIndex = 0;
+        int labelIndex = 0;
         // Start is called before the first frame update
         void Start()
         {
             resolver = GetComponent<SpriteResolver>();
-            currentSpriteLibrary = resolver.spriteLibrary.spriteLibraryAsset;
+            foreach(string cat in resolver.spriteLibrary.spriteLibraryAsset.GetCategoryNames()){
+                catList.Add(cat);
+                Debug.Log("Categories = " + cat);
+            }
+            foreach(string lab in resolver.spriteLibrary.spriteLibraryAsset.GetCategorylabelNames(catList[0])){
+                labelList.Add(lab);
+                Debug.Log("Label = " + lab);
+            }
         }
 
-        internal void UpdateSelectionChoice()
-        {
-            // var options = new List<Dropdown.OptionData>(spriteLibraries.Length);
-            // for (int i = 0; i < spriteLibraries.Length; ++i)
-            // {
-            //     options.Add(new Dropdown.OptionData(spriteLibraries[i].name));
-            // }
+        public void ChangeCategory(){ //Loop through Gritty Colors
+            if(catIndex < catList.Count - 1){
+                catIndex++;
+            } else {
+                catIndex = 0;
+            }
+            UpdateSprite();
         }
 
-        public void ToggleGritty(){
-            IEnumerator<string> catList = currentSpriteLibrary.GetCategoryNames().GetEnumerator();
-            IEnumerable<string> labelList = currentSpriteLibrary.GetCategorylabelNames(catList.Current);
-            catList.MoveNext(); //iterate to next category
-            resolver.SetCategoryAndLabel(catList.Current, "Yellow Shirt");
-            
-            // if(index < spriteLibraries.Length - 1){
-            //     index++;
-            // } else {
-            //     index = 0;
-            // }
-
-            // Debug.Log("My current Gritty is = " + spriteLibraries[index].name);
+        public void ChangeLabel(){ //Loop through Shirt Colors
+            if(labelIndex < labelList.Count - 1){
+                labelIndex++;
+            } else {
+                labelIndex = 0;
+            }
+            UpdateSprite();
         }
 
-        public void ToggleCategory(){
-
+        void UpdateSprite(){
+            resolver.SetCategoryAndLabel(catList[catIndex], labelList[labelIndex]);
         }
     }
-
 }
